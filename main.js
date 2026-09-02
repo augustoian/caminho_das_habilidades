b2e1917ad7225dabde2e434e76b1b61100930b35
augustoian <augusto.ros@escola.pr.gov.br>
2026-09-02 14:59:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,3 +14,10 @@ function iniciarDesafio() {
         alert("Digite seu nome para começar!");
         return;
     }
+
+    const numeros = Math.floor(Math.randon() * desafios.length);
+    const desafio = desafios[numeros];
+
+    document.getElementById("resposta").innerHTML=`
+    <h2> Olá ${name}</h2>`
+    `<h2> Seu Desafio é: ${desafio}</h2>`
