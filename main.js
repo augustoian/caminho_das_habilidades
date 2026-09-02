9f7b0f6e3fde0faab925a50bf7e5d214fce20077
augustoian <augusto.ros@escola.pr.gov.br>
2026-09-02 14:17:02 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const desafios =["Resolver problemas lógicos",
+    "Aprender funções novas",
+    "Encontrar padrões",
+    "Criar novas soluções",
+    "Analiasar erros"
+];
+
+function inicialDesafio(){
+    const name = document.getSelectionByid("name").value;
+
+    if (name === ""){
+        alert("digite seu nome primeiro, ccatapimbas")
+        return;
+    }
+}
